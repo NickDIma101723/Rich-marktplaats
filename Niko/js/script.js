d7cfafc82c60ce0edf36ad70c0bff3397f55987f
NickDIma101723 <101723@glr.nl>
2025-06-08 18:56:47 +0000

    Refactor code structure for improved readability and maintainability

---
+++ Niko/js/script.js
@@ -0,0 +1,6 @@
+ document.addEventListener('DOMContentLoaded', function() {
+      const video = document.querySelector('video');
+      if (video) {
+        video.playbackRate = 0.8;
+      }
+    });
